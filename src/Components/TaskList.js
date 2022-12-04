f65d5b33978bee34ba735032e326f0b3481b191f
Nick Vazquez <nmv@iastate.edu>
2022-12-03 20:25:25 -0600

    Add basics from tutorial

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,22 +1,53 @@
-import {Component} from "react";
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
 
 class TaskList extends Component {
-    state = {
-        tasks: []
+    constructor(props) {
+        super(props);
+        this.state = { tasks: [] };
+        this.remove = this.remove.bind(this);
     }
 
     async componentDidMount() {
-        const response = await fetch('/tasks');
-        const body = response.json();
-        this.setState({tasks: body});
+        await fetch('/task')
+            .then(response => response.json())
+            .then(data => this.setState({tasks: data}));
+    }
+
+    async remove(id) {
+        await fetch(`/clients/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedTasks = [...this.state.tasks].filter(i => i.id !== id);
+            this.setState({tasks: updatedTasks});
+        });
     }
 
     render() {
-        const {tasks} = this.state;
-        return(
+        const {tasks, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        const taskList = tasks.map(task => {
+            // An item returned for each task found.
+            return (
+                <div className={"placeholder"}></div>
+            )
+        });
+
+        return (
             <div className={"placeholder"}>
 
             </div>
         )
     }
 }
+export default TaskList;
